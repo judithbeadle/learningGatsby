35f0c7b4a105b2845b3dbf90e973bf6cb362c30a
Jude Beadle <info@jbeadle.de>
2021-04-16 16:06:01 +0200

    "1. Get to Know Gatsby Building Blocks"

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header.js"
+
+export default function Contact() {
+    return (
+        <div style={{ color: `teal` }}>
+            <Header headerText="Contact" />
+            <p>Contact us.</p>
+        </div>
+    )
+
+}
